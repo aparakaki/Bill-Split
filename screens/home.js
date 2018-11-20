fcf863e37e00e2c1e973666feee611c3cd145d97
Bianca Jaquez Chavez <bjaquez4@gmail.com>
2018-11-20 14:19:09 -0800

    render amount of people

--- screens/home.js
+++ screens/home.js
@@ -1,31 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
         title: 'Home',
     };
+
+    state = {
+        personCount: 2,
+        people: ["Me", "Person 1"]
+    }
+
+    addPeople = () => {
+        this.setState({ personCount: this.state.personCount + 1 })
+        var newPerson = `Person ${this.state.personCount}`
+        var peopleArr = this.state.people.concat(newPerson)
+        this.setState({ people: peopleArr })
+        console.log(this.state.people);
+    }
+
+    removePeople = () => {
+        if(this.state.personCount > 2){
+            this.setState({personCount: this.state.personCount - 1})
+            var removePeopleArray = this.state.people.slice(0, this.state.people.length-1)
+            this.setState({people: removePeopleArray})
+            console.log(this.state.people) 
+        }
+        
+    }
     render() {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
-                <View style={{alignItems:'center'}}>
-                    <Text style={styles.titleStyle}>Hello!</Text>
+
+                <View style={{ alignItems: 'center' }}>
+                    <Text style={styles.titleStyle}>Select Amount of People</Text>
+                    <Button
+                        onPress={this.removePeople}
+                        title="-"
+                    />
+                    <Text>{this.state.personCount} People</Text>
+                    <Button
+                        onPress={this.addPeople}
+                        title="+"
+                    />
+                    {this.state.people.map((element, index) => {
+                        return (
+                            <View key={index + 1}>
+                                <Text
+                                key={element}>{element}</Text>
+                                <TextInput
+                                key = {index}
+                                placeholder = "Name"></TextInput>
+                            </View>
+                                );
+                    })}
                 </View>
                 <View style={styles.container2}>
-                <View style={styles.btnDiv}>
-                    <Button
-                        onPress={() => navigate('Image')}
-                        title="See Image"
-                    />
-                    <Button
-                        onPress={() => {
-                            Alert.alert('Hey!');
-                        }}
-                        title="Press Me"
-                    />
+                    <View style={styles.btnDiv}>
+                        <Button
+                            onPress={() => navigate('Calculator')}
+                            title="Next"
+                        />
+
+                    </View>
+
+
                 </View>
-                </View>
+
+
             </View>
         );
     }
