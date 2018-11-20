4a529c7375c3634ca4676c51768cedeed4bcbfa9
Bianca Jaquez Chavez <bjaquez4@gmail.com>
2018-11-20 12:39:49 -0800

    new boiler plate

--- App.js
+++ App.js
@@ -1,77 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
-import Amount from './components/amount'
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 0,
-      text: null
-    }
-  }
-
-
-  onPressButton = () => {
-    this.setState({ count: this.state.count + 1 })
-    this.renderPeople()
-    
-  }
-
-  renderPeople = ()=> {
-    return (
-      <Text>Person</Text>
-    )
-  }
   render() {
     return (
-      
-      
-      <View style={styles.container}
-      >
-       
-        <View style={{
-          height: '20%',
-          marginTop: '20%',
-          backgroundColor: "powderblue",
-          borderRadius: 4,
-          borderWidth: 1,
-          borderColor: 'black',
-        }} >
-          <Text>{this.state.count}</Text>
-          <Button 
-          onPress={this.onPressButton}
-          title="Increase Count"
-        />
-        </View>
-
-         <TextInput
-          style={{
-            height: 50,
-            borderRadius: 4,
-            borderWidth: 1,
-            borderColor: 'black',
-            fontSize: 30
-          }}
-          placeholder="00.00"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Button title="Submit"></Button>
-        <Text>
-          People: {this.state.count}</Text>
-          <Text>Total: {this.state.text}</Text>        
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
-      
-
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // flexDirection: 'column'
-
-  }
-  
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
